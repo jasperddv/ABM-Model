0360fdcc3336579953589e8ce2218876d77197c1
jasperddv <151767135+jasperddv@users.noreply.github.com>
2023-12-20 17:26:43 +0100

    Changes 20-12
    We zijn hier net begonnen met programmeren.

---
+++ agents.py
@@ -0,0 +1,120 @@
+# Importing necessary libraries
+import random
+from mesa import Agent
+from shapely.geometry import Point
+from shapely import contains_xy
+
+# Import functions from functions.py
+from functions import generate_random_location_within_map_domain, get_flood_depth, calculate_basic_flood_damage, floodplain_multipolygon
+
+
+# Define the Households agent class
+class Households(Agent):
+    """
+    An agent representing a household in the model.
+    Each household has a flood depth attribute which is randomly assigned for demonstration purposes.
+    In a real scenario, this would be based on actual geographical data or more complex logic.
+    """
+
+    def __init__(self, unique_id, model, political_situation):
+        super().__init__(unique_id, model)
+        # Initial adaptation status set to False, determines whether an agent is going to adapt and the colour of the agent on the map
+        self.is_adapted = False
+
+        #initalise attitude to 0
+        self.household_attitude = 0
+
+        # getting flood map values
+        # Get a random location on the map
+        loc_x, loc_y = generate_random_location_within_map_domain()
+        self.location = Point(loc_x, loc_y)
+
+        # Check whether the location is within floodplain
+        # Where is this used?
+        self.in_floodplain = False
+        if contains_xy(geom=floodplain_multipolygon, x=self.location.x, y=self.location.y):
+            self.in_floodplain = True
+
+        # Get the estimated flood depth at those coordinates. 
+        # the estimated flood depth is calculated based on the flood map (i.e., past data) so this is not the actual flood depth
+        # Flood depth can be negative if the location is at a high elevation
+        self.flood_depth_estimated = get_flood_depth(corresponding_map=model.flood_map, location=self.location, band=model.band_flood_img)
+        # handle negative values of flood depth
+        if self.flood_depth_estimated < 0:
+            self.flood_depth_estimated = 0
+        
+        # calculate the estimated flood damage given the estimated flood depth. Flood damage is a factor between 0 and 1
+        self.flood_damage_estimated = calculate_basic_flood_damage(flood_depth=self.flood_depth_estimated)
+
+        # Add an attribute for the actual flood depth. This is set to zero at the beginning of the simulation since there is not flood yet
+        # and will update its value when there is a shock (i.e., actual flood). Shock happens at some point during the simulation
+        self.flood_depth_actual = 0
+        
+        #calculate the actual flood damage given the actual flood depth. Flood damage is a factor between 0 and 1
+        self.flood_damage_actual = calculate_basic_flood_damage(flood_depth=self.flood_depth_actual)
+
+        # political perception of household is determined by political situation + a random value between -0.3 and 0.3.
+        # If political perception value is above 1, it will be put to 1. If it is below 0, it is put to 0
+        self.political_perception = political_situation + random.randrange(-30, 30, 1)/100
+        if self.political_perception > 1:
+            self.political_perception = 1
+        elif self.political_perception < 0:
+            self.political_perception = 0
+
+        #create list that contains flood damage over the past years, used to create household attitude
+        self.past_flood_damages = [0, 0, 0, 0]
+        self.past_flood_damages.append(self.flood_damage_actual)
+
+    # Function to count friends who can be influencial.
+    def count_friends(self, radius):
+        """Count the number of neighbors within a given radius (number of edges away). This is social relation and not spatial"""
+        self.friends = self.model.grid.get_neighbors(self.pos, include_center=False)
+        return len(self.friends)
+
+    def step(self):
+        # Logic for adaptation based on estimated flood damage and a random chance.
+        # These conditions are examples and should be refined for real-world applications.
+        if self.flood_damage_estimated > 0.15 and random.random() < 0.2:
+            self.is_adapted = True  # Agent adapts to flooding
+
+        #append past_flood_damages list with the new actual flood damage
+        self.past_flood_damages.append(self.flood_damage_actual)
+
+        #compute household attitude using the average value of the last 5 values past_flood_damages list
+        self.household_attitude = sum(self.past_flood_damages[-5:])/5
+
+        #create list with political perception of neighbours
+        self.friends_political_perceptions_neighbours = []
+        for friend in range(len(self.friends)):
+            self.friends_political_perceptions_neighbours.append(self.friends[friend].political_perception)
+
+        #compute average poliitcal perception of neighbours
+        self.average_political_perception = sum(self.friends_political_perceptions_neighbours)/len(self.friends)
+
+        #compute new value for political perception based upon own political peeception and the average political perception of neighbours
+        self.political_perception = 0.3*self.political_perception + 0.7*self.average_political_perception_neighbours
+
+# Define the Government agent class
+class Government(Agent):
+    """
+    A government agent that currently doesn't perform any actions.
+    """
+
+    def __init__(self, unique_id, model, welfare):
+        super().__init__(unique_id, model)
+        # uitwerken
+        # budget = welfare
+
+    #added this to ensure 'agent_metrics' works in model.py. Else FriendsCount does not work in the metrics
+    def count_friends(self, radius):
+        """Count the number of neighbors within a given radius (number of edges away). This is social relation and not spatial"""
+        #Empty list, government has no friends
+        friends = []
+        return len(friends)
+
+    def step(self):
+        # The government agent doesn't perform any actions.
+        pass
+
+# More agent classes can be added here, e.g. for insurance agents.
+
---
+++ run_tests.py
@@ -0,0 +1,67 @@
+from model import AdaptationModel
+import matplotlib.pyplot as plt
+import networkx as nx
+from agents import Households, Government
+
+plt.ioff()
+
+# Initialize the Adaptation Model with 50 household agents.
+model = AdaptationModel(number_of_households=50, flood_map_choice="harvey", network="watts_strogatz") # flood_map_choice can be "harvey", "100yr", or "500yr"
+
+# Calculate positions of nodes for the network plot.
+# The spring_layout function positions nodes using a force-directed algorithm,
+# which helps visualize the structure of the social network.
+pos = nx.spring_layout(model.G)
+
+# Define a function to plot agents on the network.
+# This function takes a matplotlib axes object and the model as inputs.
+def plot_network(ax, model):
+    # Clear the current axes.
+    ax.clear()
+    # Determine the color of each node (agent) based on their adaptation status.
+    #This piece of code is an alternative for code below
+    # colors = ['blue' if agent.is_adapted else 'red' for agent in model.schedule.agents]
+    #to ensure that this piece of code is only executed for households
+    colors = []
+    for agent in model.schedule.agents:
+        if type(agent) == Households:
+            if agent.is_adapted:
+                colors.append('blue')
+            else:
+                colors.append('red')
+
+    # Draw the network with node colors and labels.
+    nx.draw(model.G, pos, node_color=colors, with_labels=True, ax=ax)
+    # Set the title of the plot with the current step number.
+    ax.set_title(f"Social Network State at Step {model.schedule.steps}", fontsize=12)
+
+# Generate the initial plots at step 0.
+# Plot the spatial distribution of agents. This is a function written in the model.py
+model.plot_model_domain_with_agents()
+
+# Plot the initial state of the social network.
+fig, ax = plt.subplots(figsize=(7, 7))
+plot_network(ax, model)
+plt.show()
+
+# Run the model for 20 steps and generate plots every 5 steps.
+for step in range(20):
+    model.step()
+
+    # Every 5 steps, generate and display plots for both the spatial distribution and network.
+    # Note the first step is step 0, so the plots will be generated at steps 4, 9, 14, and 19, which are the 5th, 10th, 15th, and 20th steps.
+    if (step + 1) % 5 == 0:
+        # Plot for the spatial map showing agent locations and adaptation status.
+        plt.figure(figsize=(10, 6))
+        model.plot_model_domain_with_agents()
+
+        # Plot for the social network showing connections and adaptation statuses.
+        fig, ax = plt.subplots(figsize=(7, 7))
+        plot_network(ax, model)
+        plt.show()
+
+agent_data = model.datacollector.get_agent_vars_dataframe()
+print(agent_data)
+
+model_data = model.datacollector.get_model_vars_dataframe()
+print(model_data)
